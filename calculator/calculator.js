f9700ac07d9cfcbec0e18237b5860fa189b39f9d
SriramJ17 <jayaramansriram95@gmail.com>
2025-10-12 19:31:00 +0530

    Simple calculator function

---
+++ calculator/calculator.js
@@ -0,0 +1,15 @@
+function addNumber(a, b) {
+    return (a + b);
+}
+function subNumber(a, b) {
+    return (a - b);
+}
+function mulNumber(a, b) {
+    return (a * b);
+}
+var addition_value = addNumber(1, 2);
+var subtraction_value = subNumber(1, 2);
+var multiplication_value = mulNumber(1, 2);
+console.log(addition_value);
+console.log(subtraction_value);
+console.log(multiplication_value);
---
+++ calculator/calculator.ts
@@ -0,0 +1,21 @@
+function addNumber(a:number,b:number) {
+    return(a+b)
+}
+
+function subNumber(a:number,b:number) {
+    return(a-b)
+}
+
+function mulNumber(a:number,b:number) {
+    return(a*b)
+}
+
+var addition_value = addNumber(1,2)
+var subtraction_value = subNumber(1,2)
+var multiplication_value = mulNumber(1,2)
+
+console.log(addition_value)
+console.log(subtraction_value)
+console.log(multiplication_value)
+
+
